4dbba022af38a18b1dfa9ebdf897225985abcd17
Luca <luca@bazzite.fritz.box>
2025-02-28 09:42:00 +0100

    added edge detection

--- entities/plinth/plinth.gd
+++ entities/plinth/plinth.gd
@@ -6,7 +6,7 @@ extends StaticBody3D
 
 var current_exhibit: Node = null
 
-@export_group("Exhibit Settings")
+## Scene file to display as the exhibit
 @export var exhibit: PackedScene:
 	get:
 		return exhibit
@@ -14,6 +14,7 @@ var current_exhibit: Node = null
 		exhibit = value
 		set_exhibit()
 
+## An optional prefix placed at the left side of the plinth
 @export var prefix: String:
 	get:
 		return prefix
@@ -21,12 +22,16 @@ var current_exhibit: Node = null
 		prefix = value
 		set_label3d()
 
+## An optional title placed on the center of the plinth
 @export var title: String:
 	get:
 		return title
 	set(value):
 		title = value
 		set_label3d()
+
+## Shortcut to the shader file of the exhibit
+@export var shader_file: Shader
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -42,9 +47,20 @@ func set_exhibit():
 	# remove the old exhibit if present
 	if current_exhibit:
 		remove_child(current_exhibit)
+		shader_file = null
 		
 	if exhibit != null:
 		var scene_node = exhibit.instantiate()
 		scene_node.position.y = 2
 		current_exhibit = scene_node
+		shader_file = get_first_shader_file(scene_node)
 		add_child(scene_node)
+
+func get_first_shader_file(node):
+	var shader_file: Shader = null
+	
+	for i: Node in node.get_children():
+		if i.mesh and i.mesh.material and i.mesh.material.shader != null:
+			shader_file = i.mesh.material.shader
+		
+	return shader_file
